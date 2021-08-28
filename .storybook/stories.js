f00ea1390202043ca10d7282a3e9763bbf57e875
Benjamin Kindle <benjaminkindle@yahoo.com>
2021-08-28 12:30:46 -0400

    Respect story parameters
    Closes #16

--- .storybook/stories.js
+++ .storybook/stories.js
@@ -4,12 +4,15 @@ import { action } from '@storybook/addon-actions'
 import RTLToggle from '../src/components/RTLToggle'
 
 storiesOf('RTLToggle', module)
+  .addParameters({ direction: 'rtl' })
   .add('default', () => (
     <RTLToggle onChange={action('change')} />
   ))
-  .add('unchecked', () => (
-    <RTLToggle checked={false} onChange={action('change')} />
-  ))
+  .add(
+    'unchecked',
+    () => (<RTLToggle checked={false} onChange={action('change')} />),
+    { direction: 'ltr' }
+  )
   .add('checked', () => (
     <RTLToggle checked onChange={action('change')} />
   ))
--- src/manager.js
+++ src/manager.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import addons from '@storybook/addons'
+import { STORY_RENDERED } from '@storybook/core-events'
 import RTLPanel from './containers/RTLPanel'
-import { ADDON_ID, PANEL_ID } from './constants'
+import { ADDON_ID, PANEL_ID, UPDATE_EVENT_ID } from './constants'
 
 export function register () {
   addons.register(ADDON_ID, (api) => {
     const channel = addons.getChannel()
+    // Whenever a story is rendered, update the state to represent the parameter value of the story.
+    // We do this here and not in the panel component because we want the parameter to be respected
+    // even if the panel is never opened
+    channel.on(STORY_RENDERED, (_) => {
+      const paramValue = api.getCurrentParameter('direction')
+      if (paramValue) {
+        channel.emit(UPDATE_EVENT_ID, { direction: paramValue })
+      }
+    })
+
     addons.addPanel(PANEL_ID, {
       title: 'RTL',
       render: ({ active, key }) => { /* eslint-disable-line react/prop-types, react/display-name */
