305cc605ae912b55a761f77a954fdc461bffbe19
Benjamin Kindle <benjaminkindle@yahoo.com>
2024-02-24 10:39:26 -0500

    remove initializeRTL

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
 // make it work with --isolatedModules
 export default {};
-
-/** @deprecated initializeRTL is no longer needed and currently does nothing. Please remove. */
-export const initializeRTL = () => {};
