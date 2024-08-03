22cb0565dd00390eac3600a32bda501f49284caf
Benjamin Kindle <benjaminkindle@yahoo.com>
2024-08-03 12:00:42 -0400

    fixup!

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,6 @@ export const TOOL_ID = `${ADDON_ID}/rtl-tool`;
 export const INITIALIZE_EVENT_ID = `${ADDON_ID}/rtl-initialize`;
 /**
  * The ID for an event that is emitted in the Storybook channel whenever an change to the direction happens.
- * The event will include.
  * Events of this type should be accompanied by a parameter with a type of {@link RTLChangeEvent}
  */
 export const UPDATE_EVENT_ID = `${ADDON_ID}/rtl-update`;
