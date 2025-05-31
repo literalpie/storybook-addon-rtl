0c6be5004a9e4bd253cf16baef8b6b36a5af128a
Benjamin Kindle <benjaminkindle@yahoo.com>
2025-05-31 09:51:19 -0400

    chore: update dependencies (#36)

--- src/components/Tool.tsx
+++ src/components/Tool.tsx
@@ -6,8 +6,14 @@ import { DirectionArrowsIcon } from "./DirectionArrowsIcon";
 export const Tool = () => {
   const [globals, setGlobals] = useGlobals();
   const storyGlobals = useStorybookApi().getStoryGlobals();
+  const title =
+    globals.addonRtl === "ltr"
+      ? "Switch direction to right-to-left."
+      : "Switch direction to left-to-right";
+
   return (
     <IconButton
+      title={title}
       // If the current story has the global set, it can not be toggled in the UI
       disabled={storyGlobals.addonRtl !== undefined}
       onClick={() => {
